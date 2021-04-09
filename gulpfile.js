c4dd2c6b67942979a1c000585cce082cc61cbae7
taro <taro.arumakan@gmail.com>
2021-04-09 20:34:47 +0900

    add deploy

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,16 @@
     // Include Gulp
 var gulp                    = require("gulp"),
 
+var deploy      = require('gulp-gh-pages');
+
+/**
+ * Push build to gh-pages
+ */
+gulp.task('deploy', function () {
+  return gulp.src("./dist/**/*")
+    .pipe(deploy())
+});
+
     // HTML plugins
     fileinclude             = require("gulp-file-include"),
     htmlmin                 = require("gulp-htmlmin"),
